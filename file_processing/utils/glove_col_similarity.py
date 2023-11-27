9b123c979605cbc2fe1cdd1025b193cc872f95dc
Jacky Luong <jacksluong@gmail.com>
2023-11-27 16:04:46 -0500

    touch grass

--- file_processing/utils/glove_col_similarity.py
+++ file_processing/utils/glove_col_similarity.py
@@ -7,7 +7,7 @@ def get_glove_embedding_space():
     """
     Load a small set of GloVe word embeddings (https://nlp.stanford.edu/projects/glove/)
     """
-    filename = "../file_processing/glove.6B.50d.txt"
+    filename = "../file_processing/glove.6B.50d.txt"  # TODO: add file to repo (currently gitignored)
     print("Reading embedding file...")
 
     with open(filename, 'r') as f:
@@ -22,7 +22,7 @@ def get_glove_embedding_space():
 
 def get_phrase_embedding(phrase, embedding_space):
     """
-    Sum embeddings for each word in `phrase` with embeddings extracted from 
+    Sum embeddings for each word in `phrase` with embeddings extracted from
     `embedding_space`
     """
 
@@ -50,17 +50,17 @@ def get_schema_header_match(schema_col, csv_headers, embedding_space):
     return the similarity between the schema column name and each column header
     in the file in descending similarity score order.
     """
-    
+
     res = []
     schema_embed = get_phrase_embedding(schema_col, embedding_space)
-        
+
     for csv_col in csv_headers:
         csv_embed = get_phrase_embedding(csv_col, embedding_space)
 
         if schema_embed is None and csv_embed is None:
             if schema_col == csv_col: # headers can be symbols - if exact match, assign similarity of 1
-                res.append((csv_col, 1.0)) 
-        
+                res.append((csv_col, 1.0))
+
         if schema_embed is not None and csv_embed is not None:
             similarity = 1 - cosine(schema_embed, csv_embed)
             res.append((csv_col, similarity))
@@ -69,7 +69,7 @@ def get_schema_header_match(schema_col, csv_headers, embedding_space):
 
 def get_all_matches(schema_headers, csv_headers, embedding_space):
     """
-    Calculate and store pairwise cosine embedding similarity between desired 
+    Calculate and store pairwise cosine embedding similarity between desired
     schema headers and a given CSV file's column headers.
 
     When both a schema and CSV header aren't words found in the embedding space,
@@ -80,31 +80,31 @@ def get_all_matches(schema_headers, csv_headers, embedding_space):
         print("WARNING: duplicate column headers in schema, ")
 
     all_matches = defaultdict(list)
-    
+
     for schema_col in schema_headers:
         if schema_col not in all_matches:
             all_matches[schema_col] = []
 
         schema_embed = get_phrase_embedding(schema_col, embedding_space)
-            
+
         for csv_col in csv_headers:
             csv_embed = get_phrase_embedding(csv_col, embedding_space)
 
             if schema_embed is None and csv_embed is None:
                 if schema_col == csv_col: # headers can be symbols - if exact match, assign similarity of 1
-                    all_matches[schema_col].append((csv_col, 1.0)) 
-            
+                    all_matches[schema_col].append((csv_col, 1.0))
+
             if schema_embed is not None and csv_embed is not None:
                 similarity = 1 - cosine(schema_embed, csv_embed)
                 all_matches[schema_col].append((csv_col, similarity))
-    
+
     return all_matches
-    
+
 
 def get_csv_matches(schema_headers, csv_headers, embedding_space):
     """
     Return a dict mapping each schema header to its best match within `csv_headers`.
-    If no match exists for a given key, None is stored.  Otherwise, values are in 
+    If no match exists for a given key, None is stored.  Otherwise, values are in
     format (header: str, cosine_simarity: int)
     """
 
@@ -131,5 +131,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
+
 
--- table_joins/join.py
+++ table_joins/join.py
@@ -4,25 +4,8 @@ import pandas as pd
 
 import sys
 sys.path.append("../")
-from file_processing.utils.glove_col_similarity import * 
+from file_processing.utils.glove_col_similarity import *
 sys.path.pop()
-
-def join_tables(desired_headers, intersection, result_filename):
-    er_info = {}
-    for file1, file2 in intersection:
-        for filename in [file1, file2]:
-                df = pd.read_csv(filename)
-                columns = df.to_dict(orient='list')
-                for cname in columns:
-                    if cname in desired_headers:
-                        er_info[cname] = columns[cname]
-
-    with open(result_filename, 'w', newline='') as f:
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(er_info.keys())
-        csv_writer.writerows(zip(*er_info.values()))
-
-    print("er columns:", er_info)
 
 def get_headers(filenames):
     headers_per_file = {}
@@ -34,6 +17,76 @@ def get_headers(filenames):
             headers_per_file[filename] = header
 
     return headers_per_file
+
+
+def join_tables(headers, files_to_matches, intersection, result_filename):
+    """
+    Given the mapping of files to the columns that they are providing information for,
+    and the intersection of the two files, join the two tables together
+    """
+
+    # create a dictionary {file: {col: schema_col}}
+    projections = {}
+    for file, matches in files_to_matches.items():
+        projections[file] = {}
+        for match in matches:
+            col, schema_col = match
+            projections[file][col] = schema_col
+
+    # create the dataframes
+    file_pair, join_cols = next(iter(intersection.items()))
+    file1, file2 = file_pair
+    df1, df2 = pd.read_csv(file1), pd.read_csv(file2)
+    jc1, jc2, _ = join_cols
+
+    # reformat the dataframes to only contain the columns that we need
+    for col in df1.columns:
+        if col not in projections[file1] and col != jc1:
+            del df1[col]
+    for col in df2.columns:
+        if col not in projections[file2] and col != jc2:
+            del df2[col]
+    df1.rename(columns=projections[file1], inplace=True)
+    df2.rename(columns=projections[file2], inplace=True)
+
+    # create a dictionary {val: [indices]} for the values in the join column of file1
+    join_col_vals1 = {}
+    for i, val in enumerate(df1[jc1]):
+        if val in join_col_vals1:
+            join_col_vals1[val].append(i)
+        else:
+            join_col_vals1[val] = [i]
+
+    # iterate through all the rows of the first file and perform hash join
+    # with the second file
+    with open(result_filename, 'w', newline='') as f:
+        csv_writer = csv.writer(f)
+
+        # write the header
+        columns = list(df1.columns) + list(df2.columns)
+        columns.remove(jc2)
+        csv_writer.writerow(columns)
+
+        # write the rows
+        # TODO: add primary key column?
+        for i, row in df2.iterrows():
+            if row[jc2] in join_col_vals1:
+                for index in join_col_vals1[row[jc2]]:
+                    columns = list(df1.iloc[index]) + list(row.drop(jc2))
+                    csv_writer.writerow(columns)
+
+
+def get_headers(filenames):
+    headers_per_file = {}
+    for filename in filenames:
+        with open(filename, 'r') as file:
+            csv_reader = csv.reader(file, delimiter="\n")
+            header = next(csv_reader)[0].split(',')
+            header = [col.replace('\ufeff', '') for col in header]
+            headers_per_file[filename] = header
+
+    return headers_per_file
+
 
 def get_best_intersection(cols1, cols2, embedding_space):
     """
@@ -52,18 +105,18 @@ def get_best_intersection(cols1, cols2, embedding_space):
             if c1 == c2: # found perfect match, return early
                 print("column names match, returning early:", c1, c2)
                 return c1, c2, 1.0
-            
+
             if c1_embedding is not None and c2_embedding is not None:
                 similarity = 1 - cosine(c1_embedding, c2_embedding)
                 if similarity > highest_similarity:
                     best_col1, best_col2, highest_similarity = c1, c2, similarity
 
     return best_col1, best_col2, highest_similarity
-                
+
 
 def find_header_intersection(csv_headers, embedding_space):
     """
-    Given the set of files + column headers that we need to join across, find the column most similar 
+    Given the set of files + column headers that we need to join across, find the column most similar
     across each pair that will be the target of the join
 
     Return format: dict mapping filenames part of the intersection to cols that most resemble each other
@@ -72,7 +125,7 @@ def find_header_intersection(csv_headers, embedding_space):
 
     filenames = [fn for fn in csv_headers.keys()]
     best_intersection = {}
-    
+
     for i in range(len(filenames)):
         cols1 = csv_headers[filenames[i]]
         for j in range(i+1, len(filenames)):
@@ -80,7 +133,7 @@ def find_header_intersection(csv_headers, embedding_space):
             best_intersection[(filenames[i], filenames[j])] = get_best_intersection(cols1, cols2, embedding_space)
 
     return best_intersection
-        
+
 
 def get_matches(schema_headers, csv_headers):
     """
@@ -97,7 +150,7 @@ def get_matches(schema_headers, csv_headers):
         matches[schema_header] = {}
         for filename, headers in csv_headers.items():
             matches[schema_header][filename] = get_schema_header_match(schema_header, headers, embedding_space)
-    
+
     cols_to_matches = {} # map schema header to (file containing best match, match col name, similarity score)
     for schema_header, match_info in matches.items():
         best_file, closest_col_name, highest_sim_score = None, None, -float('inf')
@@ -107,7 +160,7 @@ def get_matches(schema_headers, csv_headers):
             col_name, similarity_score = scores[0] # already sorted in decreasing similarity order
             if highest_sim_score < similarity_score:
                 best_file, closest_col_name, highest_sim_score = filename, col_name, similarity_score
-        
+
         cols_to_matches[schema_header] = (best_file, closest_col_name, highest_sim_score)
 
     files_to_matches = {} # map each file to the set of columns that it is providing information for (map its column name to the schema column that its matching)
@@ -118,12 +171,12 @@ def get_matches(schema_headers, csv_headers):
     return cols_to_matches, files_to_matches
 
 if __name__ == '__main__':
+    files = ["./weather.csv", "./states.csv"]
+    headers = get_headers(files)
+
     embedding_space = get_glove_embedding_space()
-    schema_headers = ["rain", "temperature"]
-    csv_headers = {
-        "file1:": ["weather", "georgia", "plaid", "tulips"],
-        "file2": ["precipitation", "celsius", "fahrenheit", "roses"]
-    }
+    schema_headers = ["rain", "temperature", "city"]
+    csv_headers = { file: headers[file] for file in files }
     cols_to_matches, files_to_matches = get_matches(schema_headers, csv_headers)
 
     print("cols to matches:", cols_to_matches)
@@ -134,3 +187,5 @@ if __name__ == '__main__':
     if len(files_to_matches) > 1: # means that best columns are in different files, must execute a join
         intersection = find_header_intersection(csv_headers, embedding_space) # assume just 2 files for now
         print("intersection:", intersection)
+
+        join_tables(headers, files_to_matches, intersection, "joined.csv")
