228b663de9713bb9e0b408b8b3f8528a43dc1e10
Nicole Stiles <ncstiles17@gmail.com>
2023-12-11 04:53:45 -0500

    fine-tuned gpt header prompt + pull random data sample instead consecutive

--- table_joins/gpt_optimizations/gpt_column_headers.py
+++ table_joins/gpt_optimizations/gpt_column_headers.py
@@ -2,37 +2,54 @@ import csv
 import os
 import openai
 import pandas as pd
+import random
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
-def get_data_sample(filepath, num_lines=20):
-    with open(filepath, "r", encoding='utf-8-sig') as f:
-        header = next(f)
+def get_data_sample(filepath, num_lines=20, random_sample=False):
+    """
+    Get the header and `num_lines` of data from the csv
+    """
 
-        data = ""
-        for _ in range(num_lines):
-            line = next(f, None)
-            if line is None:
-                break
-            data += line
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+    header = lines[0]
+    data = None
+
+    if random_sample:
+        num_lines = min(num_lines, len(lines))
+        indices = random.sample(range(1, len(lines)), num_lines)
+        data = "".join([lines[i] for i in indices])
+    else:
+        end = min(num_lines+1, len(lines))
+        data = "".join(lines[1: end])
 
     return header, data
 
 
 def get_chat_topic(filename, header, data):
+    """
+    Use the filename and data to get a GPT-generated header for the file
+    """
+
     prompt = f"""
-    Here's an excerpt from a file titled {filename}.
-    As you can tell, the column names within the csv aren't very precise. can you generate better ones for me given the data?
-    Please return it in the same format that the header is provided in below.
+    Here's an excerpt from a file titled "{filename}".
+    As you can tell, the column names within the csv don't perfectly reflect the actual content of the data. 
+    Generate a set of headers more representative of the data to replace the current header.  
+    Output the answer only as a list of comma-separated values, in the order of 
+    the columns that they should label, with no other output.
 
     Header: {header}
 
     Data: {data}
     """
 
+    print("prompt:", prompt)
+
     client = openai.OpenAI(api_key = OPENAI_API_KEY)
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[
         {"role": "system",
         "content": prompt}
@@ -49,21 +66,20 @@ def generate_csv(gpt_headers, filepath):
     df = pd.read_csv(filepath)
     df.columns = new_headers
 
-    gpt_filepath = os.path.splitext(filepath)[0] + "_gpt.csv"
-
-    # Save the DataFrame back to a new CSV file or overwrite the existing one
+    directory, filename = os.path.split(filepath)
+    gpt_filepath = os.path.join(directory, "GPT HEADER " + filename)
     df.to_csv(gpt_filepath, index=False)
+    
+    return gpt_filepath
 
+def generate_gpt_header(filepath):
+    """
+    Given a csv file, use GPT to generate a more representative set of headers
+    and save the results in a new file
+    """
 
-def main():
-    filepath = "../table_joins/women_in_parliament.csv"
-
-    header, data = get_data_sample(filepath)
+    header, data = get_data_sample(filepath, random_sample=True)
     filename = os.path.basename(filepath)
     gpt_headers = get_chat_topic(filename, header, data)
 
-    generate_csv(gpt_headers, filepath)
-
-
-if __name__ == '__main__':
-    main()
+    return generate_csv(gpt_headers, filepath)
